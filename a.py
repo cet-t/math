c7aa2f8a62d3763bc7c30fea3c5f7dd1ee3d8e73
cet <cets021@gmail.com>
2023-11-24 02:13:02 +0900

    a

--- a.py
+++ a.py
@@ -1,9 +1,10 @@
 import dataclasses
 from typing import Any, Callable
-from numpy import *
+import numpy as np
+import math
 
-r2d: float = 180 / pi
-d2r: float = pi / 180
+r2d: float = 180 / math.pi
+d2r: float = math.pi / 180
 
 
 @dataclasses.dataclass
@@ -15,7 +16,7 @@ class V:
         return f'({self.x},{self.y})'
 
     def mag(self) -> float:
-        return sqrt(self.x ** 2 + self.y ** 2)
+        return np.sqrt(self.x ** 2 + self.y ** 2)
 
     @staticmethod
     def dot(a, b) -> float:
@@ -27,14 +28,33 @@ class V:
         lal: float = a.mag()
         lbl: float = b.mag()
         dot: float = V.dot(a, b)
-        theta: float = arccos(dot / (lal * lbl))
+        theta: float = np.arccos(dot / (lal * lbl))
         return theta * r2d
 
     @staticmethod
     def polar_to_rectangular(a):
-        x: float = a.x * cos(a.y)
-        y: float = a.x * sin(a.y)
+        x: float = a.x * np. cos(a.y)
+        y: float = a.x * np. sin(a.y)
         return V(x, y)
+
+
+class mathematics:
+    __MAX_DIGIT: int = 16
+    RAD_TO_DEG: float = 180 / np.pi
+    DEG_TO_RAD: float = np.pi / 180
+
+    @staticmethod
+    def sin(theta_deg: float, digit: int = 1) -> float:
+        # 0以下なら0、__MAX_DIGIT未満ならdigit、__MAX_DIGIT以上なら__MAX_DIGIT
+        return np.sin(theta_deg * mathematics.__MAX_DIGIT) if digit <= 0 else np.round(np.sin(theta_deg * mathematics.DEG_TO_RAD), digit if digit < mathematics.__MAX_DIGIT else mathematics.__MAX_DIGIT)
+
+    @staticmethod
+    def cos(theta_deg: float, digit: int = 1) -> float:
+        return np.cos(theta_deg * mathematics.__MAX_DIGIT) if digit <= 0 else np.round(np.cos(theta_deg * mathematics.DEG_TO_RAD), digit if digit < mathematics.__MAX_DIGIT else mathematics.__MAX_DIGIT)
+
+    @staticmethod
+    def tan(theta_deg: float, digit: int = 1) -> float:
+        return np.tan(theta_deg * mathematics.__MAX_DIGIT) if digit <= 0 else np.round(np.tan(theta_deg * mathematics.DEG_TO_RAD), digit if digit < mathematics.__MAX_DIGIT else mathematics.__MAX_DIGIT)
 
 
 def deco_s_deco(func) -> Callable[..., Any]:
@@ -66,8 +86,8 @@ def angle():
     b = V(3, 1)
 
     # magnitude
-    lal: float = sqrt(a.x ** 2 + a.y ** 2)
-    lbl: float = sqrt(b.x ** 2 + b.y ** 2)
+    lal: float = np.sqrt(a.x ** 2 + a.y ** 2)
+    lbl: float = np.sqrt(b.x ** 2 + b.y ** 2)
     print('|a|:', lal)
     print('|b|:', lbl)
 
@@ -81,32 +101,32 @@ def angle():
     print(cos8)
 
     # radian to degree
-    theta = arccos(cos8) * r2d
-    theta2 = arccos(cos82) * r2d
+    theta = np.arccos(cos8) * r2d
+    theta2 = np.arccos(cos82) * r2d
     print('θ:', theta, '\nθ2:', theta2)  # cos45°
 
 
 @prefix('polor')
 def polar():
     #! x=rcosθ, y=rsinθ
-    a = V(2, pi/3)
-    ax = a.x * cos(a.y)
-    ay = a.x * sin(a.y)
+    a = V(2, math. pi/3)
+    ax = a.x * np.cos(a.y)
+    ay = a.x * np.sin(a.y)
     print('a:', V(ax, ay))  # (1, √3)
 
-    b = V(sqrt(2), pi/4)
-    bx = b.x * cos(b.y)
-    by = b.x * sin(b.y)
+    b = V(np.sqrt(2), math.pi/4)
+    bx = b.x * np. cos(b.y)
+    by = b.x * np.sin(b.y)
     print('b:', V(bx, by))  # (1, 1)
 
-    c = V(4, 5/6*pi)
-    cx = c.x * cos(c.y)
-    cy = c.x * sin(c.y)
+    c = V(4, 5/6*math.pi)
+    cx = c.x * np.cos(c.y)
+    cy = c.x * np.sin(c.y)
     print('c:', V(cx, cy))  # (-2√3, 2)
 
-    d = V(3, pi)
-    dx = d.x * cos(d.y)
-    dy = d.x * sin(d.y)
+    d = V(3, math.pi)
+    dx = d.x * np.cos(d.y)
+    dy = d.x * np.sin(d.y)
     print('d:', V(dx, dy))  # (-3, 0)
 
 
@@ -117,8 +137,8 @@ def tri() -> None:
     # 1/tan(x) = tan(90°-x)
 
     print('---', 1, '---')
-    sin1 = 1/sqrt(10)
-    cos1 = 3/sqrt(10)
+    sin1 = 1/np.sqrt(10)
+    cos1 = 3/np.sqrt(10)
     tan1 = 1/3
     print(sin1, cos1, tan1)
 
@@ -130,11 +150,11 @@ def tri() -> None:
 
     print('------')
     # sin135° = √2/2
-    print(sin(135), 1/sqrt(2))
+    print(mathematics.sin(135, 3))
     # cos135° = -(√2/2)
-    print(cos(135))
+    print(mathematics.cos(135))
     # tan135° = -1
-    print(tan(135))
+    print(mathematics.tan(135))
 
 
 if __name__ == '__main__':
